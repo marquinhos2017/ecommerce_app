d886a28e072d746b020eefc6533d8d2f0a797888
Marcos Rodrigues Correia <marcosrodriguescorreia@Marcoss-MacBook-Air.local>
2024-01-03 08:14:59 -0900

    creating more products and creating add and remove functions to cart

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -7,6 +7,37 @@ class Shop {
       price: 49.99,
       description: "Item 1 description",
       //imagePath: "imagePath")
-    )
+    ),
+    Product(
+      name: "Product 2",
+      price: 42.99,
+      description: "Item 2 description",
+      //imagePath: "imagePath")
+    ),
+    Product(
+      name: "Product 3",
+      price: 39.99,
+      description: "Item 3 description",
+      //imagePath: "imagePath")
+    ),
+    Product(
+      name: "Product 4",
+      price: 29.99,
+      description: "Item 4 description",
+      //imagePath: "imagePath")
+    ),
   ];
+
+  List<Product> _cart = [];
+
+  List<Product> get shop => _shop;
+  List<Product> get cart => _cart;
+
+  void addToCart(Product item) {
+    _cart.add(item);
+  }
+
+  void removeFromCart(Product item) {
+    _cart.remove(item);
+  }
 }
