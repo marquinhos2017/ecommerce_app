9f827149420435f130ec8fdc1cd303442d87f59d
Marcos Rodrigues Correia <marcosrodriguescorreia@Marcoss-MacBook-Air.local>
2024-01-03 09:03:13 -0900

    styling the items of shop

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -13,12 +13,48 @@ class MyProductTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(12)),
       margin: const EdgeInsets.all(10),
       padding: const EdgeInsets.all(25),
-      child: Column(children: [
-        Icon(Icons.favorite),
-        Text(product.name),
-        Text(product.description),
-        Text(product.price.toStringAsFixed(2)),
-      ]),
+      width: 300,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(25),
+                    child: Icon(Icons.favorite),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  product.name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Text(product.price.toStringAsFixed(2)),
+          ]),
     );
   }
 }
