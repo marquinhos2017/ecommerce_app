693994d19a6468b7fb9b93108af12df1a02c364d
Marcos Rodrigues Correia <marcosrodriguescorreia@Marcoss-MacBook-Air.local>
2024-01-03 21:17:10 -0900

    add anshowing the products added to cart and creating the remove button

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,11 +1,37 @@
 import 'package:ecommerce_app/components/my_drawer.dart';
+import 'package:ecommerce_app/models/product.dart';
+import 'package:ecommerce_app/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  void removeItemFromCart(BuildContext context, Product product) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Remove this item to your cart ?"),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Cancel"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              context.read<Shop>().removeFromCart(product);
+            },
+            child: Text("Yes"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<Shop>().cart;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -14,6 +40,23 @@ class CartPage extends StatelessWidget {
         title: const Text("Cart Page"),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(children: [
+        Expanded(
+            child: ListView.builder(
+                itemCount: cart.length,
+                itemBuilder: (context, index) {
+                  final item = cart[index];
+
+                  return ListTile(
+                    title: Text(item.name),
+                    subtitle: Text(item.price.toString()),
+                    trailing: IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: () => removeItemFromCart(context, item),
+                    ),
+                  );
+                }))
+      ]),
     );
   }
 }
