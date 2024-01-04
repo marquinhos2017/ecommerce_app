fbd2f1eae53d1d89f654df8921ad556b7e706a6b
Marcos Rodrigues Correia <marcosrodriguescorreia@Marcoss-MacBook-Air.local>
2024-01-03 21:08:45 -0900

    add the modal to add the product to cart

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -1,9 +1,33 @@
 import 'package:ecommerce_app/models/product.dart';
+import 'package:ecommerce_app/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyProductTile extends StatelessWidget {
   final Product product;
   const MyProductTile({super.key, required this.product});
+
+  void addToCart(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Add this item to your cart ?"),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Cancel"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              context.read<Shop>().addToCart(product);
+            },
+            child: Text("Yes"),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +77,21 @@ class MyProductTile extends StatelessWidget {
             const SizedBox(
               height: 25,
             ),
-            Text(product.price.toStringAsFixed(2)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('\$' + product.price.toStringAsFixed(2)),
+                Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: IconButton(
+                      onPressed: () => addToCart(context),
+                      icon: Icon(Icons.add),
+                    )),
+              ],
+            ),
           ]),
     );
   }
